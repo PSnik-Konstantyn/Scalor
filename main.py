bfc23670ec40b3b8fe18d60c33b6c077aecc5beb
Kostiantyn <sinkkostia@gmail.com>
2024-10-10 23:46:17 +0300

    Wow! Huge update

--- main.py
+++
@@ -1,8 +0,0 @@
-from Lexer import Lexer
-from config import lexer_config
-
-lexer = Lexer(lexer_config)
-
-with open("example.scalor", "r", encoding="utf8") as source_code:
-    tokens = lexer.analyze(source_code.read())
-    print("Tokens:", tokens)
